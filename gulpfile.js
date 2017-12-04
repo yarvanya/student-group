d02ba659e2284fa919742b884c3cdb384a083fa2
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-12-04 08:08:39 +0200

    Configured gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const connect = require('gulp-connect');
+
+gulp.task('default', function() {
+  connect.server();
+  return gulp.src([
+      './node_modules/angular/angular.min.js',
+      'app/components/**/*.js'
+    ])
+    .pipe(concat('scripts/bundle.js'))
+    .pipe(gulp.dest('out/'));
+});
